d69b35904526ad59401b3dd6e55ca9d3538a5a8c
LucianoGimenezz <luciano.gimenezz21@gmail.com>
2023-01-24 13:38:37 -0300

    [FIX] problema con el CORS

--- server/src/routes/server.js
+++ server/src/routes/server.js
@@ -3,10 +3,15 @@ const data = require('../utils/data')
 
 http.createServer(function (req, res) {
   if (req.url.includes('rickandmorty/character')) {
-    const id = req.url.split('/')[2]
-    const character = data.filter((item) => item.id === id).pop()
-    res.writeHead(200, { 'Content-Type': 'application/json' })
+    const header = {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+    }
+    const id = req.url.split('/')[req.url.split('/').length - 1]
+    const character = data.find((item) => item.id === parseInt(id))
+    res.writeHead(200, header)
     res.end(JSON.stringify(character))
+    return
   }
   res.writeHead(404, { 'Content-Type': 'text/plain' })
   res.end('Route not found')
